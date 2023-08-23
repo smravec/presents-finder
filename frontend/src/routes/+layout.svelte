dc274bcd12b508a7123a07b6b5233884f565ed50
Simon Mravec <simon.mravec@gmail.com>
2023-08-23 14:33:18 +0200

    Removed analytics

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,3 @@
-<!-- <script>
-    import { dev } from '$app/environment';
-    import { inject } from '@vercel/analytics';
- 
-    inject({ mode: dev ? 'development' : 'production' });
-</script> -->
-
-
 <style>
 :global(*){
     font-family: 'Roboto',sans-serif;
