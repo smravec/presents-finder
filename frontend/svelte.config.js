ade1729edda5c5a7df62b86dde0b1c50ef29a65e
Simon Mravec <simon.mravec@gmail.com>
2023-08-23 14:04:12 +0200

    Vercel deploy

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,8 @@
-import adapter from '@sveltejs/adapter-netlify';
+import vercel from '@sveltejs/adapter-vercel';
 
 export default {
   kit: {
-    adapter: adapter(),
+    adapter: vercel(),
     // ... other options
   },
 };
-
